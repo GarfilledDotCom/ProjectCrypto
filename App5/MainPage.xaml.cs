f78ea7090db8b4218d47477c2596544fa4d458f8
Blake <79953660+GarfilledDotCom@users.noreply.github.com>
2022-11-25 22:32:27 +1300

    adding stuff

---
+++ App5/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using RestSharp;
+using Newtonsoft.Json;
+using App5.Model;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+
+namespace App5
+{
+    public partial class MainPage : ContentPage
+    {
+        private string apikey = "68AD6A04-198E-40BC-AAF7-89217784451F";
+        private string baseImageUrl = "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_256/";
+        private List<Coin> coin;
+        public MainPage()
+        {
+            InitializeComponent();
+            coinListView.ItemsSource = Getcoin();
+
+        }
+
+
+        private void RefreshButton_Clicked(object sender, EventArgs e)
+        {
+            coinListView.ItemsSource = Getcoin();
+        }
+
+        private List<Coin> Getcoin()
+        {
+            List<Coin> coin;
+
+            var client = new RestClient("http://rest.coinapi.io/v1/assets?filter_asset_id=BTC;LTC;ETH;DOGE;KRW");
+            var request = new RestRequest();
+            request.AddHeader("X-CoinAPI-Key", apikey);
+
+            var response = client.Execute(request);
+
+            coin = JsonConvert.DeserializeObject<List<Coin>>(response.Content);
+
+            foreach(var c in coin)
+            {
+                if (!String.IsNullOrEmpty(c.Id_icon))
+                    c.Icon_Url = baseImageUrl + c.Id_icon.Replace("-", "") + ".png";
+                else
+                    c.Icon_Url = "";
+            }
+
+            return coin;
+        }
+    }
+}
---
+++ App5/Model/Coin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App5.Model
+{
+    internal class Coin
+    {
+        public string Assiet_id { get; set; }
+        public string Name { get; set; }
+        public float Price_usd { get; set; }
+        public string Id_icon { get; set; }
+        public string Icon_Url { get; set; }
+
+    }
+}
